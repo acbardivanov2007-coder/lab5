38a86d92a5a2374a8691d869304c95c646b680f8
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-10-26 21:14:14 +0700

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+ int main()
+{
+ int min, max;
+ int summ = 0;
+ int m, M;
+ printf("введите нижнийи верхний предел:\n");
+ scanf("%d %d", &min, &max);
+ while (min != max)
+        {
+         for (m=min, M=max+1; m != M; m++)
+                 summ = summ + m*m;
+         printf("сумма квадратов целых чисел от %d до %d равна %d\n", min*min, max*max, summ);
+         summ=0;                       
+         printf("введите следующую комбинацию пределов:\n");
+         scanf("%d %d", &min, &max);
+        }
+ return 0;
+ }
